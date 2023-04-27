5e1a93a1cb24b1c8f97ca7ce67e1a53c46c316ac
machumzd <www.machuzz123@gmail.com>
2023-04-27 11:12:57 +0530

    signup completed using firebase authentication and values store in firebase store

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import 'firebase/auth'
+import 'firebase/firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyBNxZfUB628u7VUnAhIcPwBJdiTX4ePPhE",
+    authDomain: "first-85b4f.firebaseapp.com",
+    projectId: "first-85b4f",
+    storageBucket: "first-85b4f.appspot.com",
+    messagingSenderId: "369655836034",
+    appId: "1:369655836034:web:a5e38431a7a4474852b39b",
+    measurementId: "G-T108NY4TX8"
+  };
+  
+  export default firebase.initializeApp(firebaseConfig)
